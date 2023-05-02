513d59cb8d341add805e22d03ba948cfb75a3177
cachho <admin@ch-webdev.com>
2023-05-03 01:38:08 +0200

    fix: catch route

--- src/redirect.ts
+++ src/redirect.ts
@@ -3,49 +3,53 @@ import { getAffiliates } from './lib/getAffiliates';
 import { validateRegisterPage } from './lib/validateRegisterPage';
 
 function redirect() {
-  getAffiliates().then((affiliates) => {
-    if (!affiliates) {
-      return null;
-    }
+  getAffiliates()
+    .then((affiliates) => {
+      if (!affiliates) {
+        return null;
+      }
 
-    //   window.addEventListener('DOMContentLoaded', () => {
-    // Wait for the page to finish loading before redirecting.
-    // NOTE: The DomContentLoaded method quit working with the new
-    // manifest, so I removed it. However, I'm sure there's a reason
-    // I used it in the first place
+      //   window.addEventListener('DOMContentLoaded', () => {
+      // Wait for the page to finish loading before redirecting.
+      // NOTE: The DomContentLoaded method quit working with the new
+      // manifest, so I removed it. However, I'm sure there's a reason
+      // I used it in the first place
 
-    // Agent related
-    const agent = detectAgent(window.location.href);
-    if (!agent) {
-      return null;
-    }
+      // Agent related
+      const agent = detectAgent(window.location.href);
+      if (!agent) {
+        return null;
+      }
 
-    if (!validateRegisterPage(agent, window.location)) {
-      return null;
-    }
+      if (!validateRegisterPage(agent, window.location)) {
+        return null;
+      }
 
-    const affiliate = affiliates.find((aff) => aff.name === agent);
-    if (!affiliate) {
-      return null;
-    }
-    // Get Url Parameters
-    const urlParams = new URLSearchParams(window.location.search);
+      const affiliate = affiliates.find((aff) => aff.name === agent);
+      if (!affiliate) {
+        return null;
+      }
+      // Get Url Parameters
+      const urlParams = new URLSearchParams(window.location.search);
 
-    // Switch between agents that use url parameters and those who use url paths
-    if (affiliate.param && affiliate.param !== '' && affiliate.ref) {
-      if (urlParams.get(affiliate.param) !== affiliate.ref) {
+      // Switch between agents that use url parameters and those who use url paths
+      if (affiliate.param && affiliate.param !== '' && affiliate.ref) {
+        if (urlParams.get(affiliate.param) !== affiliate.ref) {
+          // Check if not already applied
+          urlParams.set(affiliate.param, affiliate.ref);
+          window.location.search = urlParams.toString();
+        }
+      } else if (affiliate.ref && affiliate.param === '') {
         // Check if not already applied
-        urlParams.set(affiliate.param, affiliate.ref);
-        window.location.search = urlParams.toString();
+        if (window.location.href !== affiliate.url) {
+          window.location.href = affiliate.url;
+        }
       }
-    } else if (affiliate.ref && affiliate.param === '') {
-      // Check if not already applied
-      if (window.location.href !== affiliate.url) {
-        window.location.href = affiliate.url;
-      }
-    }
-    return true;
-  });
+      return true;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
 redirect();

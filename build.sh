9d9a45e41c8a535513c68cb68420af468d6560ec
cachho <admin@ch-webdev.com>
2023-05-02 23:34:09 +0200

    feat: output size

--- build.sh
+++ build.sh
@@ -138,5 +138,6 @@ echo "\nBuild finished"
 echo "> build/"
 echo "> dist/chromium.zip"
 echo "> dist/firefox.zip"
+find "dist" -type f -exec stat -c"%s %n" {} \; | awk '{size_in_kb = $1 / 1024; printf("%-10.2f KB %s\n", size_in_kb, $2)}'
 
 # TODO: Validate build

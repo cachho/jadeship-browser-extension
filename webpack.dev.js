ce1a5dcf3f0e65baede673353fc535f6277e840e
cachho <admin@ch-webdev.com>
2023-05-07 13:14:05 +0200

    fix: watch webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,4 +4,8 @@ const common = require('./webpack.common');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  watchOptions: {
+    poll: true,
+    ignored: '/node_modules/',
+  },
 });

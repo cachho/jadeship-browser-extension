32c918d6283dfdbf5d5508fc9764f6dfb842d70d
cachho <admin@ch-webdev.com>
2023-05-09 00:01:22 +0200

    style: background color

--- src/components/Button.ts
+++ src/components/Button.ts
@@ -4,7 +4,7 @@ export const Button = (href: string, newTab?: boolean) => {
   button.style.border = '2px solid';
   button.style.padding = '8px';
   button.style.borderColor = '#FFFFFF';
-  button.style.background = 'linear-gradient(90deg, #9c28b0 0%, #c659d9 100%)';
+  button.style.background = 'linear-gradient(90deg, #c659d9 0%, #F679d9 100%)';
   button.style.color = '#FFFFFF';
   button.style.textDecoration = 'none';
   if (newTab) {

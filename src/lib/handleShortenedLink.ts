40c6ad1f978c081c89b6f4d6b83294af136923d1
cachho <admin@ch-webdev.com>
2023-07-09 18:05:42 +0200

    fix: update to new url

--- src/lib/handleShortenedLink.ts
+++ src/lib/handleShortenedLink.ts
@@ -6,13 +6,13 @@ export async function handleShortenedLink(
   settings: Settings
 ): Promise<URL | null> {
   if (link.hostname === 'pandabuy.page.link' && settings.onlineFeatures) {
-    const url = `https://api.reparchive.com/convert-pandabuy${link.pathname}`;
+    const url = `https://api.reparchive.com/convert/pandabuy${link.pathname}`;
     const response: ApiResponse<{ url: string }> = await fetchData(url);
     if (response && response.data) {
       return new URL(response.data.url);
     }
   } else if (link.hostname === 'qr.1688.com' && settings.onlineFeatures) {
-    const url = `https://api.ch-webdev.com/convert-1688/${link.pathname.slice(
+    const url = `https://api.reparchive.com/convert/1688/${link.pathname.slice(
       3
     )}`;
     const response: ApiResponse<{ url: string }> = await fetchData(url);

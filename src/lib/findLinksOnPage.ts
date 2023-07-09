c94a6cd9bc8efbd97e883793e2d64937650ef375
cachho <admin@ch-webdev.com>
2023-07-09 18:36:04 +0200

    refactor: move content editable check to other filters

--- src/lib/findLinksOnPage.ts
+++ src/lib/findLinksOnPage.ts
@@ -58,11 +58,12 @@ export function findLinksOnPage(settings: Settings) {
     return isInsideContentEditable(element.parentElement);
   }
 
-  const links = Array.from(document.querySelectorAll('a')).filter(
-    (a) => !isInsideContentEditable(a)
+  const links = Array.from(
+    document.querySelectorAll('a')
   ) as HTMLAnchorElement[];
 
   return links
+    .filter((a) => !isInsideContentEditable(a))
     .filter((a) => targetedHrefs.some((href) => a.href.indexOf(href) !== -1))
     .filter((a) => a.dataset.reparchiveExtension !== 'true')
     .filter((a) => !a.href.startsWith('https://qc.photos'));

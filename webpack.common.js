4e8111be021aa4976a0a659fe81e12f4a3ccd89a
cachho <admin@ch-webdev.com>
2023-09-28 17:11:32 +0200

    fix: file reference

--- webpack.common.js
+++ webpack.common.js
@@ -18,7 +18,6 @@ module.exports = {
     index: './src/index.tsx',
     background: './src/background.ts',
     content_script: './src/content_script.ts',
-    popup: './src/popup.ts',
     redirect: './src/redirect.ts',
     toolbar: './src/toolbar.ts',
   },

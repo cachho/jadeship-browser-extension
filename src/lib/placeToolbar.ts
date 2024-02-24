465d66bd37c806fa0a68e35931f20404bc335917
cachho <admin@ch-webdev.com>
2024-02-25 00:04:41 +0100

    style: pandabuy sticky

--- src/lib/placeToolbar.ts
+++ src/lib/placeToolbar.ts
@@ -20,6 +20,20 @@ export function placeToolbar(
         targetElement.appendChild(elem);
       }
     });
+  }
+  if (agent === 'pandabuy') {
+    // Select the target element with the class 'home-fixed'
+    const query = '.global-content';
+    waitForElement(query, (targetElement) => {
+      // Check if the target element has a first child
+      if (targetElement.firstChild) {
+        // If there is a first child, insert the element before it
+        targetElement.insertBefore(elem, targetElement.firstChild);
+      } else {
+        // If there is no first child, append the element to the target element
+        targetElement.appendChild(elem);
+      }
+    });
   } else {
     body.insertAdjacentElement('afterbegin', elem);
   }

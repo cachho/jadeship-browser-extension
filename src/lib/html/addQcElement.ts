e474981755fd9d4c1d8b693730354da39e7d608e
cachho <admin@ch-webdev.com>
2024-08-08 23:47:56 +0200

    fix: findsly link

--- src/lib/html/addQcElement.ts
+++ src/lib/html/addQcElement.ts
@@ -1,8 +1,14 @@
 import type { CnLink } from 'cn-links';
 
+import { findslyMappings } from '../api/getQcAvailable';
+
 export function addQcElement(cnLink: CnLink, link: HTMLAnchorElement) {
   const elem = document.createElement('a');
-  const url = new URL('https://finds.ly/product');
+  const url = new URL(
+    `https://finds.ly/product/${findslyMappings.get(cnLink.marketplace)}/${
+      cnLink.id
+    }`
+  );
   url.searchParams.set('url', cnLink.as('raw').href);
   elem.href = url.href;
   elem.target = '_blank';

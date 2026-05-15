d7a832f7f69665d26500d48c29199580cfd9dafc
wx <dominik.sladecek07@gmail.com>
2026-05-15 21:48:25 +0200

    refactor(ui): complete redesign, fix issues from previous PR

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import './App.css';
+import "./App.css";
 
-import Popup from './components/Popup';
+import Popup from "./components/Popup";
 
 function App() {
-  return (
-    <div className="App">
-      <Popup />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Popup />
+		</div>
+	);
 }
 
 export default App;

45a944fd6a6170efcf748be23a1aa71c9a9ab1a5
cachho <admin@ch-webdev.com>
2026-05-19 09:09:47 +0200

    fix: link

--- src/lib/getStatsUrl.ts
+++ src/lib/getStatsUrl.ts
@@ -4,5 +4,5 @@ import type { CnLink } from "../models";
 export function getStatsUrl(cnLink: CnLink | null): string | null {
   if (!cnLink) return null;
   const origin = new URL(Config.endpoint.details).origin;
-  return `${origin}/${cnLink.marketplace}/${cnLink.id}?r=extension`;
+  return `${origin}/item/${cnLink.marketplace}/${cnLink.id}?r=extension`;
 }

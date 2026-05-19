b326e12164db78b682f33db82801a72738f1e03e
cachho <admin@ch-webdev.com>
2026-05-19 12:19:00 +0200

    chore: linting

--- src/__tests__/initializeExtension.test.ts
+++ src/__tests__/initializeExtension.test.ts
@@ -4,27 +4,27 @@ import { isStoredValueEqual } from "../lib/initializeExtension";
 import { defaultSettings } from "../models/Settings";
 
 describe("isStoredValueEqual", () => {
-	test("matches primitive settings values", () => {
-		expect(isStoredValueEqual(true, true)).toBe(true);
-		expect(
-			isStoredValueEqual(defaultSettings.myAgent, defaultSettings.myAgent),
-		).toBe(true);
-	});
+  test("matches primitive settings values", () => {
+    expect(isStoredValueEqual(true, true)).toBe(true);
+    expect(
+      isStoredValueEqual(defaultSettings.myAgent, defaultSettings.myAgent),
+    ).toBe(true);
+  });
 
-	test("matches array settings values by content", () => {
-		const storedValue = [...defaultSettings.agentsInToolbar];
-		expect(storedValue).not.toBe(defaultSettings.agentsInToolbar);
-		expect(
-			isStoredValueEqual(storedValue, defaultSettings.agentsInToolbar),
-		).toBe(true);
-	});
+  test("matches array settings values by content", () => {
+    const storedValue = [...defaultSettings.agentsInToolbar];
+    expect(storedValue).not.toBe(defaultSettings.agentsInToolbar);
+    expect(
+      isStoredValueEqual(storedValue, defaultSettings.agentsInToolbar),
+    ).toBe(true);
+  });
 
-	test("does not match different array values", () => {
-		expect(
-			isStoredValueEqual(
-				[...defaultSettings.agentsInToolbar, "cnfans"],
-				defaultSettings.agentsInToolbar,
-			),
-		).toBe(false);
-	});
+  test("does not match different array values", () => {
+    expect(
+      isStoredValueEqual(
+        [...defaultSettings.agentsInToolbar, "cnfans"],
+        defaultSettings.agentsInToolbar,
+      ),
+    ).toBe(false);
+  });
 });
